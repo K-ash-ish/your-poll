c38acab02e8ed79554e23c8333bfa7446039f40e
K-ash-ish <kashishsondhiya@gmail.com>
2022-12-21 10:19:15 +0530

    style changes

--- src/components/VoteFor.jsx
+++ src/components/VoteFor.jsx
@@ -3,11 +3,10 @@ function VoteFor(props) {
   const { optionNO, option } = props;
   return (
     <div className="options my-4 ">
-      <div className="option md:text-xl  flex items-center border-b-2  pb-4">
-        <p className=" w-28 ">Option {optionNO}:</p>
-        <div className="vote  w-full flex justify-between items-center">
-          <p className=" ml-6">{option}</p>
-          <button className="btn-vote border-2 px-3 py-1 font-medium ">
+      <div className="option md:text-xl  flex items-center  pb-4">
+        <div className=" vote w-full flex justify-between items-center">
+          <p className=" mx-6  w-full bg-cyan-100 hover:bg-cyan-200 transitions-color duration-300 ease-in p-4">{option}</p>
+          <button className=" py-3 px-6 font-semibold text-sm bg-cyan-300 text-white rounded-md shadow-sm hover:text-black ease-in duration-200 ">
             Vote
           </button>
         </div>
--- src/components/YourPoll.jsx
+++ src/components/YourPoll.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import VoteFor from "./VoteFor";
 function YourPoll() {
   return (
-    <div className="w-full min-h-screen my-10 flex justify-center  items-center">
-      <div className="poll border-2  rounded p-6 md:w-1/2  my-2">
-        <p className="md:text-2xl text-xl underline decoration-red-500 underline-offset-8 mb-10 md:mb-14">
+    <div className=" w-full min-h-screen my-10 flex justify-center  items-center">
+      <div className="bg-slate-50 poll border-2 border-cyan-100 rounded md:p-6 py-4 px-2 md:w-1/2 w-11/12  my-2">
+        <p className="md:text-2xl text-xl underline decoration-red-500 underline-offset-8 px-2 mb-10 md:mb-14">
           What is your height?
         </p>
         <VoteFor optionNO="1" option="Height is 1.2m" />

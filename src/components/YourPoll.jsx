7062acfb9aae5233cc21b36d352800220d533a58
K-ash-ish <kashishsondhiya@gmail.com>
2022-12-21 09:43:41 +0530

    rename

--- src/components/YourPoll.jsx
+++ src/components/YourPoll.jsx
@@ -3,7 +3,7 @@ import VoteFor from "./VoteFor";
 function YourPoll() {
   return (
     <div className="w-full min-h-screen my-10 flex justify-center  items-center">
-      <div className="poll border-2 border-red-300 rounded p-6 md:w-1/2  my-2">
+      <div className="poll border-2  rounded p-6 md:w-1/2  my-2">
         <p className="md:text-2xl text-xl underline decoration-red-500 underline-offset-8 mb-10 md:mb-14">
           What is your height?
         </p>

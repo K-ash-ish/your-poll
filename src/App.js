adeff75ad750c22f7878acf9f0a0df6626a120fe
K-ash-ish <kashishsondhiya@gmail.com>
2023-01-30 20:42:42 +0530

    now when clicking vote button showing the results page with updated votes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import { Route, Routes } from "react-router-dom";
 import YourPoll from "./components/YourPoll";
 import NotFound from "./components/NotFound";
+import Result from "./components/Result";
 function App() {
   return (
     <div className="container box-border flex flex-col flex-wrap items-center mb-4 ">
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/yourpoll">
           <Route path=":pollid" element={<YourPoll />} />
+          <Route path=":pollid/results" element={<Result />} />
           {/* relative path beacuse else it will open the route from root */}
         </Route>
         <Route path="*" element={<NotFound />} />
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,36 +1,81 @@
 import React from "react";
-function Result() {
-  function OptionResult(props) {
-    const { option, vote } = props;
-    return (
-      <div className="option-result m-4 p-2 border-2 ">
-        <div className="md:text-xl border-b-2 option font-medium">{option}</div>
-        <div className="votes font-bold m-2">
-          <span className="text-sky-500">Votes: </span>
-          {vote}
-        </div>
-        <div className="vote-bar m-2 border-y-2 h-4 rounded-md flex justify-start items-center">
-          <div className="w-1/2 option-vote h-4  rounded-md bg-rose-500"></div>
-        </div>
-      </div>
-    );
-  }
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+import NotFound from "./NotFound";
+function OptionResult(props) {
+  const { option, vote } = props;
   return (
-    <div className="result-container border-2 w-full  my-10 flex flex-col justify-center  items-center">
-      <h2 className="text-2xl md:text-4xl font-bold tracking-wider mb-8 underline decoration-rose-500 underline-offset-4">
-        Result
-      </h2>
-      <div className="results w-4/5 md:w-1/2 min-h-max border-2 border-cyan-100 rounded flex flex-col">
-        <OptionResult vote="24" option="Height is 1.24." />
-        <OptionResult vote="24" option="Height is 1.10." />
-        <OptionResult vote="24" option="Height is 1.54." />
+    <div className="option-result m-4 p-2 border-2 ">
+      <div className="md:text-xl border-b-2 option font-medium">{option}</div>
+      <div className="votes font-bold m-2">
+        <span className="text-sky-500">Votes: </span>
+        {vote}
       </div>
-      {/* <div className="share-link border-2 w-4/5 flex  justify-around m-4">
+      <div className="vote-bar m-2 border-y-2 h-4 rounded-md flex justify-start items-center">
+        <div className="w-1/2 option-vote h-4  rounded-md bg-rose-500"></div>
+      </div>
+    </div>
+  );
+}
+function Result() {
+  const currentPoll = useSelector((state) => state.question);
+  // const currentPoll = {
+  //   id: "acb4d890-d5be-4cc3-aa5d-f9aa8f76a74f",
+  //   question: "asqw",
+  //   options: [
+  //     {
+  //       option: "qaszx",
+  //       vote: 0,
+  //     },
+  //     {
+  //       option: "zxzxzx",
+  //       vote: 0,
+  //     },
+  //     {
+  //       option: "qaszx",
+  //       vote: 0,
+  //     },
+  //     {
+  //       option: "zxzxzx",
+  //       vote: 0,
+  //     },
+  //   ],
+  // };
+
+  const { pollid } = useParams();
+  // will have to change this search from all polls
+  return (
+    <>
+      {currentPoll.id === pollid ? (
+        <div className="result-container border-2 w-full  my-10 flex flex-col justify-center  items-center">
+          <h2 className="text-2xl md:text-4xl font-bold tracking-wider mb-8 underline decoration-rose-500 underline-offset-4">
+            Result
+          </h2>
+          <div className="results w-4/5 md:w-1/2 min-h-max border-2 border-cyan-100 rounded flex flex-col">
+            {currentPoll.options.map((poll) => {
+              return (
+                <OptionResult
+                  key={uuidv4()}
+                  vote={poll.vote}
+                  option={poll.option}
+                />
+              );
+              {
+                /* no need tochange */
+              }
+            })}
+          </div>
+          {/* <div className="share-link border-2 w-4/5 flex  justify-around m-4">
         <button>Share link</button>
         <button>Share link</button>
         <button>Share link</button>
       </div> */}
-    </div>
+        </div>
+      ) : (
+        <NotFound />
+      )}
+    </>
   );
 }
 

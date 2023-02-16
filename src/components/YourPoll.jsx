37183526e77b8c5ffa62751fea1417df73fe789c
K-ash-ish <kashishsondhiya@gmail.com>
2023-02-16 23:41:48 +0530

    added qr code for sharing the poll

--- src/components/YourPoll.jsx
+++ src/components/YourPoll.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { useParams } from "react-router-dom";
+import QRCode from "react-qr-code";
 import VoteFor from "./VoteFor";
 import Header from "./Header";
 import { pollCollectionRef } from "../firebase-config";
@@ -18,6 +19,7 @@ function YourPoll() {
       setPoll(data.data());
     });
   };
+
   // will have to change this and get from all polls
   // const [pollResult, setPollResult] = useState();
   // const newPoll = JSON.parse(localStorage.getItem("newPoll")) || "";
@@ -25,15 +27,15 @@ function YourPoll() {
 
   // localStorage.setItem("newPoll", JSON.stringify(newPoll));
   return !poll?.id ? (
-    <React.Fragment >
+    <React.Fragment>
       <h1 className="my-6 text-2xl">Loading...</h1>
       <p>(Check your link. If taking too much time)</p>
     </React.Fragment>
   ) : (
     <>
       <Header />
-      <div className=" w-full my-6 flex justify-center  items-center">
-        <div className="bg-slate-50 poll border-2 border-cyan-100 rounded md:p-6 py-4 px-2 md:w-1/2 w-11/12  my-2">
+      <div className="  w-full my-6 flex flex-col items-center justify-center md:flex-row md:justify-around ">
+        <div className="bg-slate-50 poll border-2 border-cyan-100  rounded md:p-6 py-4 px-2 md:w-1/2 w-11/12 my-2">
           <p className=" border-b-4 border-red-400 tracking-wide leading-relaxed md:text-2xl text-xl pb-2 px-2 mb-10 md:mb-14">
             {poll.question} ?
           </p>
@@ -46,6 +48,14 @@ function YourPoll() {
               />
             );
           })}
+        </div>
+        <div className="sharing-links flex flex-col items-center h-18  justify-center my-4  ">
+          <QRCode
+            size={256}
+            style={{ height: "auto", width: "50%" }}
+            value={`https://your-poll.netlify.app/yourpoll/${pollid}`}
+            viewBox={`0 0 256 256`}
+          />
         </div>
       </div>
     </>

ebbf1a98ba86e18df95d581936f29c8fcfdfd8d9
K-ash-ish <kashishsondhiya@gmail.com>
2023-02-19 19:30:15 +0530

    removed redux toolkit from app

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from "react";
 import "./index.css";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
         <App />
-      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );

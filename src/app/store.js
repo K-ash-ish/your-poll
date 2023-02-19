d141145c57d210aad28b6750138448c8f98659e5
K-ash-ish <kashishsondhiya@gmail.com>
2023-02-19 19:28:55 +0530

    removed unuused packages

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import questionReducer from "../features/question/qustionSlice";
-import pollReducer from "../features/poll/pollSlice";
-export const store = configureStore({
-  reducer: {
-    question: questionReducer,
-    allPoll: pollReducer,
-  },
-});

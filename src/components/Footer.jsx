c1b36167bb8e82a2d54ba64762943d64565d0bf2
K-ash-ish <kashishsondhiya@gmail.com>
2023-02-20 20:00:45 +0530

    anchor tag fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,24 +4,35 @@ import {
   faLinkedin,
   faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
-import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer className="relative  my-4 w-full  bottom-0 flex  justify-around items-center">
       <div className=" social-links text-4xl my-1  text-sky-500">
-        <Link className=" px-6" to="https://github.com/K-ash-ish">
-          <FontAwesomeIcon icon={faGithub} />
-        </Link>
-        <Link
+        <a
           className=" px-6"
-          to="https://www.linkedin.com/in/kashish-sondhiya-969120198/"
+          href="https://github.com/K-ash-ish"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a
+          className=" px-6"
+          href="https://www.linkedin.com/in/kashish-sondhiya-969120198/"
+          rel="noopener noreferrer"
+          target="_blank"
         >
           <FontAwesomeIcon icon={faLinkedin} />
-        </Link>
-        <Link className=" px-6" to="https://twitter.com/KashishSo">
+        </a>
+        <a
+          className=" px-6"
+          href="https://twitter.com/KashishSo"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
           <FontAwesomeIcon icon={faTwitter} />
-        </Link>
+        </a>
       </div>
     </footer>
   );

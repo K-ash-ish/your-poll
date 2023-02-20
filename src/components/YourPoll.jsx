b676089e25a09f27c9d4cd2786e03425fcaadc1b
K-ash-ish <kashishsondhiya@gmail.com>
2023-02-20 20:34:09 +0530

    view results button on your poll page

--- src/components/YourPoll.jsx
+++ src/components/YourPoll.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import QRCode from "react-qr-code";
 import VoteFor from "./VoteFor";
 import { pollCollectionRef } from "../firebase-config";
@@ -34,8 +34,14 @@ function YourPoll() {
   ) : (
     <div className="  w-full my-6 flex flex-col items-center justify-center md:flex-row md:justify-around ">
       <div className="bg-slate-50 poll border-2 border-cyan-100  rounded md:p-6 py-4 px-2 md:w-1/2 w-11/12 my-2">
-        <p className=" border-b-4 border-red-400 tracking-wide leading-relaxed md:text-2xl text-xl pb-2 px-2 mb-10 md:mb-14">
+        <p className=" flex felx-row justify-between items-center border-b-4 border-red-400 tracking-wide leading-relaxed md:text-2xl text-xl pb-2 px-2 mb-10 md:mb-14">
           {poll.question} ?
+          <Link
+            to={`/yourpoll/${poll.id}/results`}
+            className="my-2 text-lg font-bold underline"
+          >
+            <p>Results</p>
+          </Link>
         </p>
         {poll.option.map((option, index) => {
           return (
@@ -48,12 +54,13 @@ function YourPoll() {
           );
         })}
       </div>
-      <div className="sharing-links flex flex-col items-center h-18  justify-center my-4  ">
+      <div className="sharing-links flex flex-col border-2 items-center p-2 h-18  justify-center my-4  ">
         <QRCode
           size={256}
           style={{ height: "auto", width: "50%" }}
           value={`https://your-poll.netlify.app/yourpoll/${pollid}`}
           viewBox={`0 0 256 256`}
+          className="pt-2"
         />
       </div>
     </div>

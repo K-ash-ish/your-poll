e83ad7117db4ea628d06345fae4c032d9f3b6691
K-ash-ish <kashishsondhiya@gmail.com>
2023-02-20 19:10:05 +0530

    footer completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <div className="fixed mb-4 w-full  bottom-0 flex  justify-center items-center social-links text-4xl my-1  text-sky-500">
+      <Link className=" px-6" href="https://github.com/K-ash-ish">
+        <FontAwesomeIcon icon={faGithub} />
+      </Link>
+      <Link
+        className=" px-6"
+        to="https://www.linkedin.com/in/kashish-sondhiya-969120198/"
+      >
+        <FontAwesomeIcon icon={faLinkedin} />
+      </Link>
+      <Link className=" px-6" to="https://twitter.com/KashishSo">
+        <FontAwesomeIcon icon={faTwitter} />
+      </Link>
+    </div>
+  );
+}
+
+export default Footer;
